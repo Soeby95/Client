da9af69aeb62755adca32085536e71238b1c0e3d
Soeby95 <chmi16ab@student.cbs.dk>
2017-11-07 17:37:50 +0100

    Implementering af bootstrap & clickmetode til login knappen
    Implementerer bootstrap og en clickmetode fra eksemplet fra undervisningen. Clickmetode tilpasses min egen client og testes.

---
+++ js/login.js
@@ -0,0 +1,25 @@
+$(document).ready(() => {
+
+    // SDK.User.loadNav();
+
+    $("#login-button").click(() => {
+
+        console.log("clicked");
+
+       /* const email = $("#inputEmail").val();
+        const password = $("#inputPassword").val();
+
+        SDK.User.login(email, password, (err, data) => {
+            if (err && err.xhr.status === 401) {
+                $(".form-group").addClass("has-error");
+            }
+            else if (err){
+                console.log("Fejl")
+            } else {
+                window.location.href = "my-page.html";
+            }*/
+        });
+
+    });
+
+});
