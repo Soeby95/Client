d7e76c8d4c1ece3b12b2f67d43d6f456ab808f6a
Soeby95 <chmi16ab@student.cbs.dk>
2017-11-13 20:59:51 +0100

    Clickmetode virker

--- js/sdk.js
+++ js/sdk.js
@@ -1,5 +1,5 @@
 const SDK = {
-    serverURL: "http://dis-bookstore.herokuapp.com/api",
+    serverURL: "https://localhost:8443/api",
     request: (options, cb) => {
         $.ajax({
             url: SDK.serverURL + options.url,
@@ -18,54 +18,56 @@ const SDK = {
     },
 
 
-Users: {
-    findAll: (cb) => {
-        SDK.request({method: "GET", url: "/users"}, cb);
-    },
-    current: () => {
-        return SDK.Storage.load("users");
-    },
-    logOut: () => {
-        SDK.Storage.remove("tokenId");
-        SDK.Storage.remove("userId");
-        SDK.Storage.remove("user");
-        window.location.href = "index.html";
-    },
-    login: (email, password, cb) => {
-        SDK.request({
-            data: {
-                email: email,
-                password: password
-            },
-            url: "/users/login?include=user",
-            method: "POST"
-        }, (err, data) => {
+    Users: {
+        findAll: (cb) => {
+            SDK.request({method: "GET", url: "/users"}, cb);
+        },
+        current: () => {
+            return SDK.Storage.load("users");
+        },
+        logOut: () => {
+            SDK.Storage.remove("tokenId");
+            SDK.Storage.remove("userId");
+            SDK.Storage.remove("user");
+            window.location.href = "index.html";
+        },
+        login: (email, password, cb) => {
+            SDK.request({
+                data: {
+                    email: email,
+                    password: password
+                },
+                url: "/users/login?include=user",
+                method: "POST"
+            }, (err, data) => {
 
-            //On login-error
-            if (err) return cb(err);
+                //On login-error
+                if (err) return cb(err);
 
-        SDK.Storage.persist("tokenId", data.id);
-        SDK.Storage.persist("userId", data.userId);
-        SDK.Storage.persist("user", data.user);
+                SDK.Storage.persist("tokenId", data.id);
+                SDK.Storage.persist("userId", data.userId);
+                SDK.Storage.persist("user", data.user);
 
-        cb(null, data);
+                cb(null, data);
 
-    });
-    },
-    loadNav: (cb) => {
-        $("#nav-container").load("nav.html", () => {
-            const currentUser = SDK.User.current();
-        if (currentUser) {
-            $(".navbar-right").html(`
+            });
+        },
+        loadNav: (cb) => {
+            $("#nav-container").load("nav.html", () => {
+                const currentUser = SDK.User.current();
+                if (currentUser) {
+                    $(".navbar-right").html(`
             <li><a href="my-page.html">Your orders</a></li>
             <li><a href="#" id="logout-link">Logout</a></li>
           `);
-        } else {
-            $(".navbar-right").html(`
+                } else {
+                    $(".navbar-right").html(`
             <li><a href="login.html">Log-in <span class="sr-only">(current)</span></a></li>
           `);
+                }
+                $("#logout-link").click(() => SDK.User.logOut());
+                cb && cb();
+            });
         }
-        $("#logout-link").click(() => SDK.User.logOut());
-        cb && cb();
-    });
     }
+}
