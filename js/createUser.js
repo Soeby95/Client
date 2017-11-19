148cb0e90ef0647703baeb6707e405c08b9d98ea
Soeby95 <chmi16ab@student.cbs.dk>
2017-11-19 20:33:47 +0100

    JS fil til createUser
    JS fil til knappen createUser

---
+++ js/createUser.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+
+    SDK.Users.loadNav();
+
+    $("#createUser-button").click(() => {
+
+        const password = $("#inputPassword").val();
+        const name = $("#inputName").val();
+        const lastname = $("#inputLastName").val();
+        const email = $("#inputEmail").val();
+        const description = $("#inputDescription").val();
+        const gender = $("#inputGender").val();
+        const major = $("#inputMajor").val();
+        const semester = $("#inputSemester").val();
+
+        SDK.Users.login(password,name,lastname,email,description,gender,major,semester (err, data) => {
+            if (err && err.xhr.status === 401) {
+                $(".form-group").addClass("has-error");
+            }
+            else if (err){
+                console.log("Fejl")
+            } else {
+                window.location.href = "login.html";
+            }
+        });
+
+    });
+
+});
+
